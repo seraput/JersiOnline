ce960fca764af60836edca31b58ddf725bacbbab
unknown <septian@merindo.co.id>
2022-10-27 16:31:12 +0700

    add testing

---
+++ testing.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Testing = () => {
+  return (
+    <View>
+      <Text>Hello Broo</Text>
+    </View>
+  );
+};
+
+export default Testing;
