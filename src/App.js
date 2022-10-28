6aa38dd204c0c4124f1fe76d0d93778a6aa513a9
Septian <septian@merindo.co.id>
2022-10-28 16:59:32 +0700

    2.4

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './pages/Home';
+import Splash from './pages/Splash';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/pages/Splash/index.js
@@ -0,0 +1,14 @@
+import {Text, StyleSheet, View} from 'react-native';
+import React, {Component} from 'react';
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Splash Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
